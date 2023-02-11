45f4f08ac73433b52518746c487d2c57098d655a
crisTalipapa <36330477+crisTalipapa@users.noreply.github.com>
2023-02-11 22:06:29 +0800

    Update BrotherController.java

--- src/main/java/com/gatesolution/BrotherController.java
+++ src/main/java/com/gatesolution/BrotherController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins="http://192.168.1.11:3000/")
+@CrossOrigin(origins="https://springbootrest-production.up.railway.app/")
 @RestController
 @RequestMapping("railway")
 public class BrotherController {
